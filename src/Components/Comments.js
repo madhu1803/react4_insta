937296beb949c06d44c57c790ee36045209c5d87
madhumithaa <madhumithaa1411@gmail.com>
2020-08-20 14:13:31 +0530

    heart done

--- src/Components/Comments.js
+++ src/Components/Comments.js
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
 import { Card } from "react-bootstrap";
+import "../Components/Css/Comments.css";
 import { AiOutlineHeart } from "react-icons/ai";
+import { findAllByTestId } from "@testing-library/react";
 
 export default class Comments extends Component {
+  state = {
+    heart: false,
+  };
+  heartHandler = () => {
+    this.setState({
+      ...this.state,
+      heart: !this.state.heart,
+    });
+  };
   render() {
     let { text, author } = this.props;
+
     return (
       <Card.Text>
         <p className="mt-2">
           <strong>{author}</strong> {text}{" "}
-          <AiOutlineHeart style={{ float: "right" }} />
+          <AiOutlineHeart
+            style={{ float: "right" }}
+            className={`${this.state.heart ? "heart-icon" : null}`}
+            onClick={this.heartHandler}
+          />
         </p>
       </Card.Text>
     );
